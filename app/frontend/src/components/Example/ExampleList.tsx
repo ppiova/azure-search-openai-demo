a5667eab4f7af2e20db2eb6d4b5f305065ae41e5
Pablito Piova <ppiova@gmail.com>
2024-06-25 12:56:17 -0300

    Add OZ Brand

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -3,9 +3,9 @@ import { Example } from "./Example";
 import styles from "./Example.module.css";
 
 const DEFAULT_EXAMPLES: string[] = [
-    "What is included in my Northwind Health Plus plan that is not in standard?",
+    "What is included in my Polaris Health Plus plan that is not in standard?",
     "What happens in a performance review?",
-    "What does a Product Manager do?"
+    "What does a Manager of Human Resources do?"
 ];
 
 const GPT4V_EXAMPLES: string[] = [
